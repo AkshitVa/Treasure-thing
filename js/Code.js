b42de63e207dd768e7c7dd3058e99b5cae301c30
AkshitVa <89950777+AkshitVa@users.noreply.github.com>
2022-02-21 17:57:33 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "BOREDOM";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R E B O D M O", 100,250)
+    fill("lightblue")
+    text("Hint: The state of feeling bored.", 100,270)
+
+}
